8cf037b8d067d5fb4d66cbc4cbd11beed8ecdbd3
Atharva Ajit Keny <8001998@kfintech.com>
2024-01-24 14:54:10 +0530

    checking commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 // import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Signup  from './Signup';
 import Login from './Login';
 import Register from './Register';
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
