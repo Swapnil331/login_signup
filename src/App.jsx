34450ca7a37573e18588bbf833d40301ff988bce
Atharva Ajit Keny <8001998@kfintech.com>
2024-01-24 15:05:25 +0530

    added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import './App.css'
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import Login from './Login';
 import Register from './Register';
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import Dashboard from './Dashboard';
 
 function App() {
   const router = createBrowserRouter([
-   {
-    path : "/",
-    element:<Register/>
-   },
-   {
-    path : "/login",
-    element:<Login/>
-   },
-   {
-    path : "/dashboard",
-    element:<Dashboard/>
-   }
-    ])
+    {
+      path: '/',
+      element: <Register />
+    },
+    {
+      path: '/login',
+      element: <Login />
+    },
+    {
+      path: '/dashboard',
+      element: <Dashboard />
+    }
+  ]);
 
   return (
     <>
-     <>
-      <RouterProvider router={router}/>
+      <>
+        <RouterProvider router={router} />
+      </>
     </>
-
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
